f1c5e9c7b7507d53f166bdf899735dc080eeb123
chillwave <brandonvaldivia21@outlook.com>
2017-12-17 04:05:46 -0500

    JoyRyde inital commit

---
+++ joyryde.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import obd
+from obd import OBDStatus
+
+ODB_PORT = "/dev/rfcomm2" # Enter COM port here, e.g /dev/rfcomm0
+
+print("####################### Starting... #######################")
+try:
+    connection = obd.OBD(ODB_PORT) # auto-connects to USB or RF port
+    # self.fail('message')
+    print("Status: " + connection)
+except Exception as e:
+    print("Setting up inital connection... (Adapter: " + OBDStatus.ELM_CONNECTED + ", Car: " + OBDStatus.CAR_CONNECTED + ")")
+    while OBDStatus.CAR_CONNECTED != "Car Connected":
+        try:
+            connection
+            print("Status:" + connection)
+        except Exception as e:
+            print("Initial connection failed, trying to reconnect... (Adapter: " + OBDStatus.ELM_CONNECTED + ", Car: " + OBDStatus.CAR_CONNECTED + ")")
+
+if OBDStatus.CAR_CONNECTED == "Car Connected":
+    print("####################### Successful link established #######################")
+
+
+def queryCar( title, queries ):
+    print("####################### " + title + " #######################")
+    for _query in queries:
+        try:
+            cmd = obd.commands[_query] # select an OBD command (sensor)
+            response = connection.query(cmd) # send the command, and parse the response
+            if 'None' in str(response.value):
+                print(_query + ": Unsupported for this car")
+                pass
+            else:
+                print(_query + ": ", end='') # returns unit-bearing values thanks to Pint
+                print(response.value)
+        except Exception as e:
+            print('Error reading [' + _query + ']')
+            print(e)
+
+# Engine
+queryCar('Engine', ['RPM','SPEED','FUEL_LEVEL'])
+
+# Accelerator
+queryCar('Accelerator', ['THROTTLE_POS','RELATIVE_THROTTLE_POS','THROTTLE_POS_B','THROTTLE_POS_C','ACCELERATOR_POS_D','ACCELERATOR_POS_E','ACCELERATOR_POS_F','THROTTLE_ACTUATOR','RELATIVE_ACCEL_POS'])
+
+# Emissions
+queryCar('Emissions', ['O2_SENSORS','O2_SENSORS_ALT','AIR_STATUS','O2_S1_WR_VOLTAGE','O2_S2_WR_VOLTAGE','O2_S3_WR_VOLTAGE','O2_S4_WR_VOLTAGE','O2_S5_WR_VOLTAGE','O2_S6_WR_VOLTAGE','O2_S7_WR_VOLTAGE','O2_S8_WR_VOLTAGE','O2_S1_WR_CURRENT','O2_S2_WR_CURRENT','O2_S3_WR_CURRENT','O2_S4_WR_CURRENT','O2_S5_WR_CURRENT','O2_S6_WR_CURRENT','O2_S7_WR_CURRENT','O2_S8_WR_CURRENT'])
+
+# Pneumatics
+queryCar('Pneumatics', ['AMBIANT_AIR_TEMP','MAX_MAF','COOLANT_TEMP','INTAKE_TEMP','INTAKE_PRESSURE','FUEL_PRESSURE','EVAP_VAPOR_PRESSURE','EVAP_VAPOR_PRESSURE_ABS','EVAP_VAPOR_PRESSURE_ALT','BAROMETRIC_PRESSURE','WARMUPS_SINCE_DTC_CLEAR','FUEL_RAIL_PRESSURE_VAC','FUEL_RAIL_PRESSURE_DIRECT'])
+
+
+
+#print(response.value.to("f")) # user-friendly unit conversions
